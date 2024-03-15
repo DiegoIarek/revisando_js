38966301a50c94fa29ce6dd8a832fe8e46461933
DiegoIarek <137222245+DiegoIarek@users.noreply.github.com>
2024-03-15 12:09:46 -0300

    Add files via upload

---
+++ revisando_js-main/revisando.js/tipo-booleano.js
@@ -0,0 +1,9 @@
+let cidade = "Campo Largo";
+let cidadeNatal = "campo largo";
+
+cidade=cidade.toLocaleLowerCase;
+cidadeNatal=cidadeNatal.toLocaleLowerCase;
+
+console.log(cidade===cidadeNatal);
+
+
